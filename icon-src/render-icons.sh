25198ff76c9e5dcd653cbc9b52b6aa969183aec2
Mirian Margiani <mirian@margiani.ch>
2020-04-25 14:17:40 +0200

    Replace app icon by more "sailfishy" variant

--- icon-src/render-icons.sh
+++ icon-src/render-icons.sh
@@ -17,23 +17,23 @@
 # along with harbour-captains-log.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# echo "rendering app icon..."
-#
-# postfix=""
-# root="../icons"
-# appicons=(harbour-captains-log)
-# for i in 86 108 128 172; do
-#     mkdir -p "$root/${i}x$i"
-#
-#     for a in "${appicons[@]}"; do
-#         if [[ ! "$a.svg" -nt "$root/${i}x$i/$a$postfix.png" ]]; then
-#             echo "nothing to do for $a at ${i}x$i"
-#             continue
-#         fi
-#
-#         inkscape -z -e "$root/${i}x$i/$a$postfix.png" -w "$i" -h "$i" "$a.svg"
-#     done
-# done
+echo "rendering app icon..."
+
+postfix=""
+root="../icons"
+appicons=(harbour-captains-log)
+for i in 86 108 128 172; do
+    mkdir -p "$root/${i}x$i"
+
+    for a in "${appicons[@]}"; do
+        if [[ ! "$a.svg" -nt "$root/${i}x$i/$a$postfix.png" ]]; then
+            echo "nothing to do for $a at ${i}x$i"
+            continue
+        fi
+
+        inkscape -z -e "$root/${i}x$i/$a$postfix.png" -w "$i" -h "$i" "$a.svg"
+    done
+done
 
 
 echo "rendering icons..."
