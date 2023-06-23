fc72d58baa15566884c320d35c49226b2091c201
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2023-06-23 19:36:32 +0200

    Implement selectable SFPM in C++

--- src/qmltypes.cpp
+++ src/qmltypes.cpp
@@ -9,6 +9,8 @@
 #include <QCoreApplication>
 
 void registerQmlTypes() {
+    qmlRegisterType<qqsfpm::SelectableSortFilterProxyModel>(
+        "SortFilterProxyModel", 0, 2, "SelectableSortFilterProxyModel");
     qmlRegisterType<qqsfpm::TagsFilter>("SortFilterProxyModel", 0, 2, "TagsFilter");
 }
 
--- src/tagsfilter.h
+++ src/tagsfilter.h
@@ -8,9 +8,67 @@
 #define TAGSFILTER_H
 
 #include "../libs/SortFilterProxyModel/filters/rolefilter.h"
+#include "../libs/SortFilterProxyModel/qqmlsortfilterproxymodel.h"
+#include "../libs/SortFilterProxyModel/proxyroles/singlerole.h"
+#include "property_macros.h"
+
 #include <QVariant>
+#include <QMap>
+#include <QVariantList>
 
 namespace qqsfpm {
+
+class SelectableSortFilterProxyModel;
+class IsSelectedRole : public SingleRole {
+    Q_OBJECT
+
+public:
+    IsSelectedRole(SelectableSortFilterProxyModel* selectionModel,
+                   QObject* parent = 0);
+
+private:
+    SelectableSortFilterProxyModel* m_selectionModel {nullptr};
+
+    virtual QVariant data(const QModelIndex &sourceIndex,
+                          const QQmlSortFilterProxyModel &proxyModel) override;
+};
+
+
+class SelectableSortFilterProxyModel : public QQmlSortFilterProxyModel {
+    Q_OBJECT
+    RW_PROPERTY(QString, selectionKey, SelectionKey, )
+    RO_PROPERTY(QVariantList, selectedKeys, )
+    RO_PROPERTY(int, selectedCount, 0)
+
+public:
+    SelectableSortFilterProxyModel(QObject* parent = 0);
+
+    bool isSourceIndexSelected(const QModelIndex& sourceIndex) const;
+    int getKeyRole() const;
+
+signals:
+    void itemSelected(const QVariant& key);
+    void itemDeselected(const QVariant& key);
+
+public slots:
+    void selectItem(const QModelIndex& index);
+    Q_INVOKABLE void selectItem(int row);
+
+    void deselectItem(const QModelIndex& index);
+    Q_INVOKABLE void deselectItem(int row);
+
+    void toggleSelection(const QModelIndex& index);
+    Q_INVOKABLE void toggleSelection(int row);
+
+    void selectAll();
+    void clearCurrent();
+    void clearAll();
+
+private:
+    QMap<QVariant, bool> m_selectedMap;
+    QSharedPointer<IsSelectedRole> m_isSelectedRole {new IsSelectedRole(this)};
+};
+
 
 class TagsFilter : public RoleFilter {
     Q_OBJECT
